b606ba39592c621dfc1f454a5b2742ea78573328
Martin Hughes <martinph01@gmail.com>
2024-03-11 12:17:38 +0000

    Updated footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,6 +4,7 @@ import {Container, Flex, Text} from "@radix-ui/themes";
 import moment from "moment";
 import Link from "next/link";
 import LogoWhite from "@/app/components/LogoWhite";
+import {FaBuilding, FaGithub, FaLinkedin, FaPhoneAlt} from "react-icons/fa";
 
 const morePages = [
     {href: "/privacy", text: "Privacy policy"},
@@ -13,7 +14,7 @@ const morePages = [
 export default function Footer() {
     return (
         <Container>
-            <div className={"flex p-5 gap-5 md:gap-10 text-white text-sm flex-col md:flex-row md:items-center"}>
+            <div className={"flex p-5 gap-5 md:gap-10 text-white text-sm flex-col md:flex-row md:items-start"}>
                 <div>
                     <LogoWhite className={"w-24"}/>
                 </div>
@@ -24,8 +25,21 @@ export default function Footer() {
                         ))}
                     </ul>
                 </div>
-                <div>
-                    © Waypoint3 Ltd {moment().format('Y')}. All Rights Reserved.
+                <div className={"flex flex-col gap-2 items-end"}>
+                    <div className={"flex items-center gap-4"}>
+                        <Link href={"https://www.linkedin.com/company/waypoint3"} target={"_blank"} aria-label={"Visit the Waypoint3 LinkedIn page"}>
+                            <FaLinkedin className={"w-8 h-8"}/>
+                        </Link>
+                        <Link href={"https://github.com/waypoint3"} target={"_blank"} aria-label={"Visit the Waypoint3 GitHub page"}>
+                            <FaGithub className={"w-8 h-8"}/>
+                        </Link>
+                    </div>
+                    <div>
+                        © Waypoint3 Ltd {moment().format('Y')}. All Rights Reserved.
+                    </div>
+                    <div>
+                        Reg No. 14023873 (England)
+                    </div>
                 </div>
             </div>
         </Container>
